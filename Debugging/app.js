667931d2002854cfd810e9bc5423304a126ab898
Uday <budayteja009@gmail.com>
2024-01-31 18:00:14 +0530

    Trash

--- Debugging/app.js
+++
@@ -1,6 +0,0 @@
-console.clear();
-console.log(`Debugger Attached`);
-
-require('http')
-    .createServer((req, res) => res.end("Hello World\n"))
-    .listen(4000);
--- notes.md
+++
@@ -1,5 +0,0 @@
-REPL
-R -> Read User Input
-E -> Evaluate User Input
-P -> Print Output (Result)
-L -> Wait for new input
